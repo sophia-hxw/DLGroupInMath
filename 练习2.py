6c9d793c28fb63dcfc45b1e7136117d4e0e8adef
xieyao2022 <2411332755@qq.com>
2022-11-18 15:26:47 +0800

    v8

--- 练习2.py
+++ 练习2.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 blank_image = np.zeros((400,532,3), np.uint8)
 blank_image.fill(255)
-img = cv2.imread('cat3.jpg')
+img = cv2.imread('wulin.jpeg')
 x1,y1 = img.shape[:2]
 print(img.shape)
 print(blank_image.shape)
@@ -11,7 +11,7 @@ if x1>400 and y1<=532:
     y2 = 400/img.shape[0]
     dim1 = (int(img.shape[1]*y2),400)
     img2 = cv2.resize(img,dim1)
-    #print(img2.shape)
+    print(img2.shape)
     x2,y2 = img2.shape[:2]
     yoff2 = ((x0 - x2) // 2)
     xoff2 = ((y0 - y2) // 2)
@@ -21,7 +21,7 @@ elif x1<=400 and y1>532:
     x3 = 532/img.shape[1]
     dim2 = (532,int(img.shape[0]*x3))
     img3 = cv2.resize(img,dim2)
-    #print(img3.shape)
+    print(img3.shape)
     x3,y3 = img3.shape[:2]
     yoff3 = ((x0 - x3) // 2)
     xoff3 = ((y0 - y3) // 2)
@@ -38,11 +38,11 @@ elif x1 > 400 and y1 > 532:
     result = blank_image.copy()
     result[yoff4:yoff4 + x4, xoff4:xoff4 + y4] = img4
 else:
-    yoff1 = ((x0 - x1) / 2)
-    xoff1 = ((y0 - y1) / 2)
+    yoff1 = ((x0 - x1) // 2)
+    xoff1 = ((y0 - y1) // 2)
     result = blank_image.copy()
     result[yoff1:yoff1 + x1, xoff1:xoff1 + y1] = img
-cv2.imshow('i', blank_image)
+#cv2.imshow('i', blank_image)
 cv2.imshow('result',result)
 cv2.imshow('img', img)
 cv2.waitKey(0)
